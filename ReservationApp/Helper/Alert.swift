18dc17ef094818bedc988a98b44212250767f585
Erick Manrique <erickmanrique@Ericks-MBP.fios-router.home>
2018-04-02 08:00:35 -0400

    first commit

---
+++ ReservationApp/Helper/Alert.swift
@@ -0,0 +1,9 @@
+//
+//  Alert.swift
+//  ReservationApp
+//
+//  Created by Erick Manrique on 4/2/18.
+//  Copyright © 2018 Erick Manrique. All rights reserved.
+//
+
+import Foundation
---
+++ ReservationApp/Model/Response.swift
@@ -0,0 +1,9 @@
+//
+//  Response.swift
+//  ReservationApp
+//
+//  Created by Erick Manrique on 4/2/18.
+//  Copyright © 2018 Erick Manrique. All rights reserved.
+//
+
+import Foundation
